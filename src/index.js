5aa75e67fc6bef23d3a8d604061f76ae5c114936
csi <robin256@naver.com>
2020-09-17 21:47:00 +0900

    Second version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
+//  컴포넌트는 하나만
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +15,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
