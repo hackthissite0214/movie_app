4e667a285f3a8aa2f19694975bcfe30f0e564ed4
csi <robin256@naver.com>
2020-09-19 21:15:09 +0900

    Updates

---
+++ precache-manifest.8423044800359993f3e0960995ab9241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2816c133162a068e1c27ec79eb94d3f1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "42545ba44dec598b3489",
+    "url": "/movie_app/static/css/main.663130f8.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42545ba44dec598b3489",
+    "url": "/movie_app/static/js/main.cf2335af.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
